5b2384e698bd6192a2f2f9e9603136038506b69a
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-01-11 13:49:46 +0900

    SensorUdpプロジェクトを作成。

---
+++ SensorUdp/src/jp/ac/ehime_u/cite/sasaki/SensorUdp/SensorUdp.java
@@ -0,0 +1,13 @@
+package jp.ac.ehime_u.cite.sasaki.SensorUdp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SensorUdp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
