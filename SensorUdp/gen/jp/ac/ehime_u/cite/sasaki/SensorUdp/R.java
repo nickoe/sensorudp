727fe412faa4abbb0b6b7f294f7d0bc1a58dece8
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-01-12 16:20:17 +0900

    SensorUdp において DatagramSocket オブジェクトをメンバ変数にした。

--- SensorUdp/gen/jp/ac/ehime_u/cite/sasaki/SensorUdp/R.java
+++ SensorUdp/gen/jp/ac/ehime_u/cite/sasaki/SensorUdp/R.java
@@ -18,15 +18,27 @@ public final class R {
         public static final int EditTextDebugMessage=0x7f05000c;
         public static final int EditTextHost=0x7f050003;
         public static final int EditTextPort=0x7f050006;
+        public static final int LinearLayoutAccelerometer=0x7f05000e;
         public static final int LinearLayoutBroadcastAddress=0x7f050007;
         public static final int LinearLayoutHost=0x7f050001;
+        public static final int LinearLayoutMagneticField=0x7f050012;
         public static final int LinearLayoutMessage=0x7f05000b;
+        public static final int LinearLayoutOrientation=0x7f050016;
         public static final int LinearLayoutPort=0x7f050004;
         public static final int SpinnerBroadcastAddress=0x7f050009;
         public static final int TextView01=0x7f050002;
         public static final int TextView02=0x7f05000a;
+        public static final int TextViewAccelerometer1=0x7f05000f;
+        public static final int TextViewAccelerometer2=0x7f050010;
+        public static final int TextViewAccelerometer3=0x7f050011;
         public static final int TextViewBroadcast=0x7f050008;
         public static final int TextViewHello=0x7f050000;
+        public static final int TextViewMagneticField1=0x7f050013;
+        public static final int TextViewMagneticField2=0x7f050014;
+        public static final int TextViewMagneticField3=0x7f050015;
+        public static final int TextViewOrientation1=0x7f050017;
+        public static final int TextViewOrientation2=0x7f050018;
+        public static final int TextViewOrientation3=0x7f050019;
         public static final int TextViewPort=0x7f050005;
     }
     public static final class layout {
--- SensorUdp/src/jp/ac/ehime_u/cite/sasaki/SensorUdp/SensorUdp.java
+++ SensorUdp/src/jp/ac/ehime_u/cite/sasaki/SensorUdp/SensorUdp.java
@@ -6,7 +6,12 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.text.DecimalFormat;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -16,10 +21,27 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class SensorUdp extends Activity implements OnClickListener {
+@SuppressWarnings("deprecation")
+public class SensorUdp extends Activity implements OnClickListener,
+		SensorListener {
 	private String destination_host;
 	private int destination_port;
+	// 本物のセンターを使う場合
+	private SensorManager sensorManager;
+	private TextView textViewAccelerometer1;
+	// センサーシミュレータを使う場合
+	// private SensorManagerSimulator sensorManager;
+	private TextView textViewAccelerometer2;
+	private TextView textViewAccelerometer3;
+	private TextView textViewMagneticField1;
+	private TextView textViewMagneticField2;
+	private TextView textViewMagneticField3;
+	private TextView textViewOrientation1;
+	private TextView textViewOrientation2;
+	private TextView textViewOrientation3;
+	private DatagramSocket datagramSocket;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -29,22 +51,45 @@ public class SensorUdp extends Activity implements OnClickListener {
 		Button button_send_udp = (Button) this
 				.findViewById(R.id.ButtonSendToggle);
 		button_send_udp.setOnClickListener(this);
-		
-		//スピナーの設定
-		
+
+		// スピナーの設定
+
 		try {
 			Inet4Addresses inet4_addresses = new Inet4Addresses();
-			ArrayAdapter<String> array_adapter = new ArrayAdapter<String>(this, R.layout.spinner, inet4_addresses.getBroadcastAddresses() );
+			ArrayAdapter<String> array_adapter = new ArrayAdapter<String>(this,
+					R.layout.spinner, inet4_addresses.getBroadcastAddresses());
 			Spinner spinner = (Spinner) findViewById(R.id.SpinnerBroadcastAddress);
 			spinner.setAdapter(array_adapter);
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			Log.v("SensorUdp#onCreate", "SocketException");
 		} catch (VerifyError e) {
 			Log.v("SensorUdp#onCreate", e.toString());
 		}
+
+		// センサー情報表示用ビューの取得
+		textViewAccelerometer1 = (TextView) findViewById(R.id.TextViewAccelerometer1);
+		textViewAccelerometer2 = (TextView) findViewById(R.id.TextViewAccelerometer2);
+		textViewAccelerometer3 = (TextView) findViewById(R.id.TextViewAccelerometer3);
+		textViewMagneticField1 = (TextView) findViewById(R.id.TextViewMagneticField1);
+		textViewMagneticField2 = (TextView) findViewById(R.id.TextViewMagneticField2);
+		textViewMagneticField3 = (TextView) findViewById(R.id.TextViewMagneticField3);
+		textViewOrientation1 = (TextView) findViewById(R.id.TextViewOrientation1);
+		textViewOrientation2 = (TextView) findViewById(R.id.TextViewOrientation2);
+		textViewOrientation3 = (TextView) findViewById(R.id.TextViewOrientation3);
+
+		// センサーの設定
+		InitSensorManager();
 		
+		// ソケットを用意
+		try {
+			datagramSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			//e.printStackTrace();
+			Log.v("SensorUdp#onCreate", e.toString());
+		}
+
 	}
 
 	public void onClick(View v) {
@@ -75,11 +120,64 @@ public class SensorUdp extends Activity implements OnClickListener {
 			InetAddress inet_address = InetAddress.getByName(destination_host);
 			DatagramPacket datagram_packet = new DatagramPacket(byte_array,
 					byte_array.length, inet_address, destination_port);
-			DatagramSocket datagram_socket = new DatagramSocket();
-			datagram_socket.send(datagram_packet);
-			datagram_socket.close();
+			//DatagramSocket datagram_socket = new DatagramSocket();
+			if(null != datagramSocket){
+				//datagramSocket.close();
+				datagramSocket = null;
+				datagramSocket = new DatagramSocket();
+			}
+			datagramSocket.send(datagram_packet);
 		} catch (IOException io_exception) {
 			Log.v("SensorUdp#SendDebugMessageByUdp", io_exception.toString());
 		}
+	}
+
+	private void InitSensorManager() {
+		// 本物のセンターを使う場合
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		// センサーシミュレータを使う場合
+		// sensorManager = SensorManagerSimulator.getSystemService(context,
+		// Context.SENSOR_SERVICE);
+		// sensorManager.connectSimulator();
+
+		sensorManager.registerListener(this, SensorManager.SENSOR_ACCELEROMETER
+				| SensorManager.SENSOR_MAGNETIC_FIELD
+				| SensorManager.SENSOR_ORIENTATION,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		// SensorManager.SENSOR_DELAY_FASTEST 最高速度
+		// SensorManager.SENSOR_DELAY_GAME ゲーム速度
+		// SensorManager.SENSOR_DELAY_NORMAL 通常速度
+		// SensorManager.SENSOR_DELAY_UI UI速度
+	}
+
+	public void onAccuracyChanged(int i, int j) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onSensorChanged(int sensor, float[] values) {
+		// 10進数固定小数点表示するためのフォーマットを行うクラス DecimalFormat
+		DecimalFormat df = new DecimalFormat("000.0000000");
+		switch (sensor) {
+		case SensorManager.SENSOR_ACCELEROMETER: {
+			// 加速度センサーの値を表示
+			textViewAccelerometer1.setText("" + df.format(values[0]));
+			textViewAccelerometer2.setText("" + df.format(values[1]));
+			textViewAccelerometer3.setText("" + df.format(values[2]));
+		}
+			break;
+		case SensorManager.SENSOR_MAGNETIC_FIELD: {
+			// 磁気センサーの値を表示
+			textViewMagneticField1.setText("" + df.format(values[0]));
+			textViewMagneticField2.setText("" + df.format(values[1]));
+			textViewMagneticField3.setText("" + df.format(values[2]));
+		}
+			break;
+		case SensorManager.SENSOR_ORIENTATION: {
+			textViewOrientation1.setText("" + df.format(values[0]));
+			textViewOrientation2.setText("" + df.format(values[1]));
+			textViewOrientation3.setText("" + df.format(values[2]));
+		}
+			break;
+		}// switchの終わり
 	}
 }
