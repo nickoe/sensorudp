d6f5e54234c736d155eb0a9a576bfc7bf2cb9999
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-01-18 12:19:03 +0900

    Swingを使用しないReceiveUdp.javaは以前のものに戻した。

--- ReceiveUdp/src/jp/ac/ehime_u/cite/sasaki/ReceiveUdp/ReceiveUdp.java
+++ ReceiveUdp/src/jp/ac/ehime_u/cite/sasaki/ReceiveUdp/ReceiveUdp.java
@@ -1,57 +1,11 @@
 package jp.ac.ehime_u.cite.sasaki.ReceiveUdp;
 
-import java.awt.FlowLayout;
 import java.net.*;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-@SuppressWarnings("serial")
-public class ReceiveUdp extends JFrame {
+public class ReceiveUdp {
 	private static final int PORT = 12345;
 
-	private final static String TITLE = "ReceiveUdp by Takashi SASAKI";
-	private final static String OPENING = "ReceiveUdp v1.0 by Takashi SASAKI, 2011\n"
-			+ "Listening all interfaces for " +PORT+"/udp.\n"
-			+ "-------------------------------------------\n";
-	private JTextArea jTextArea;
-	private JLabel jLabel;
-
-	public ReceiveUdp() {
-		// フレームにレイアウトを設定
-		getContentPane().setLayout(new FlowLayout());
-
-		// テキストエリアを作成
-		jTextArea = new JTextArea(OPENING, 30, 40) {
-			// appendメソッドをオーバーライドして常に最終行が表示されるようにする
-			@Override
-			public void append(String str) {
-				super.append(str);
-				jTextArea.setCaretPosition(jTextArea.getDocument().getLength());
-			}
-		};
-		jTextArea.setLineWrap(true);
-
-		// フレームにスクロールペインを設定
-		JScrollPane j_scroll_pane = new JScrollPane(jTextArea);
-
-		getContentPane().add(j_scroll_pane);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle(TITLE);
-		// setSize(300, 400);
-		pack();
-		setVisible(true);
-		jLabel = new JLabel();
-		getContentPane().add(jLabel);
-		jLabel.setText("" + PORT);
-	}
-
 	public static void main(String[] argv) throws Exception {
-		// Swingのフレームを表示
-		ReceiveUdp receive_udp = new ReceiveUdp();
-
 		// ワイルドカードアドレスで待ち受け
 		DatagramSocket datagram_socket = new DatagramSocket(PORT);
 
@@ -68,13 +22,9 @@ public class ReceiveUdp extends JFrame {
 			int sender_port = datagram_packet.getPort();
 			String received_data = new String(datagram_packet.getData(), 0,
 					datagram_packet.getLength());
-			// 表示する文字列を作成
-			String s = "[" + sender_address + ":" + sender_port + "]"
-					+ received_data;
 			// 受信したデータを標準出力へ出力
-			System.out.println(s);
-			// JTextAreaにも表示
-			receive_udp.jTextArea.append(s);
+			System.out.println("[" + sender_address + ":" + sender_port + "]"
+					+ received_data);
 		}
 	}
 }
