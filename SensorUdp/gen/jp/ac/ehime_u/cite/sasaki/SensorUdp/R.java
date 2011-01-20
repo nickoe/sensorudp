e87865ed854df5104cc22da58539259f0618e113
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-01-20 16:31:56 +0900

    任意のメッセージを送信するボタンを画面の一番下に移動した。

--- SensorUdp/gen/jp/ac/ehime_u/cite/sasaki/SensorUdp/R.java
+++ SensorUdp/gen/jp/ac/ehime_u/cite/sasaki/SensorUdp/R.java
@@ -14,29 +14,29 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ButtonSendToggle=0x7f050009;
-        public static final int CheckBoxAccelerometer=0x7f05000b;
-        public static final int CheckBoxMagneticField=0x7f05000e;
-        public static final int CheckBoxOrientation=0x7f050011;
-        public static final int EditTextDebugMessage=0x7f050008;
+        public static final int ButtonSendToggle=0x7f050017;
+        public static final int CheckBoxAccelerometer=0x7f050008;
+        public static final int CheckBoxMagneticField=0x7f05000b;
+        public static final int CheckBoxOrientation=0x7f05000e;
+        public static final int EditTextDebugMessage=0x7f050016;
         public static final int EditTextHost=0x7f050003;
         public static final int EditTextPort=0x7f050006;
-        public static final int LinearLayoutAccelerometer=0x7f05000a;
+        public static final int LinearLayoutAccelerometer=0x7f050007;
         public static final int LinearLayoutHost=0x7f050001;
-        public static final int LinearLayoutMagneticField=0x7f05000d;
-        public static final int LinearLayoutMessage=0x7f050007;
-        public static final int LinearLayoutOrientation=0x7f050010;
+        public static final int LinearLayoutMagneticField=0x7f05000a;
+        public static final int LinearLayoutMessage=0x7f050015;
+        public static final int LinearLayoutOrientation=0x7f05000d;
         public static final int LinearLayoutPort=0x7f050004;
-        public static final int RadioButtonFastest=0x7f050014;
-        public static final int RadioButtonGame=0x7f050015;
-        public static final int RadioButtonNormal=0x7f050016;
-        public static final int RadioButtonUi=0x7f050017;
-        public static final int RadioGroupDelay=0x7f050013;
+        public static final int RadioButtonFastest=0x7f050011;
+        public static final int RadioButtonGame=0x7f050012;
+        public static final int RadioButtonNormal=0x7f050013;
+        public static final int RadioButtonUi=0x7f050014;
+        public static final int RadioGroupDelay=0x7f050010;
         public static final int TextView01=0x7f050002;
-        public static final int TextViewAccelerometer=0x7f05000c;
+        public static final int TextViewAccelerometer=0x7f050009;
         public static final int TextViewHello=0x7f050000;
-        public static final int TextViewMagneticField=0x7f05000f;
-        public static final int TextViewOrientation=0x7f050012;
+        public static final int TextViewMagneticField=0x7f05000c;
+        public static final int TextViewOrientation=0x7f05000f;
         public static final int TextViewPort=0x7f050005;
     }
     public static final class layout {
